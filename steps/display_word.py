715ae2fc4e80df23549c58fc8812f2470888bfe0
Eva <105744651+kaevka@users.noreply.github.com>
2023-10-13 00:41:18 +0300

    Steps вывода слов

---
+++ steps/display_word.py
@@ -0,0 +1,31 @@
+class PoleChudesGame:
+    def __init__(self, word, topic, hints):
+        self.word = word.upper()
+        self.hidden_word = ['_' if char.isalpha() else char for char in self.word]
+        self.topic = topic
+        self.hints = hints
+        self.guessed_letters = set()
+        self.player1_score = 0
+        self.player2_score = 0
+        self.current_turn_score = 0
+        self.total_score = 0
+        self.player_turn = 1  # Игрок 1 начинает
+
+    def display_word(self):
+        return ' '.join(self.hidden_word)
+
+import unittest
+
+class TestPoleChudesGame(unittest.TestCase):
+    def setUp(self):
+        # Подготовка объекта PoleChudesGame для тестирования
+        self.game = PoleChudesGame("PYTHON", "Programming", ["High-level", "Interpreted"])
+
+    def test_display_word_initial(self):
+        # Проверка отображения слова перед угадыванием букв
+        self.assertEqual(self.game.display_word(), "_ _ _ _ _ _")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
